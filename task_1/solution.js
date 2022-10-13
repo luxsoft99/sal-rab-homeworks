c9f7faa530a23db740bf8f6802cc25d4b752483b
Rgdft <terra.alexey@gmail.com>
2022-10-13 19:52:43 +0600

    Task 1.2 and 1.3 solved

--- task_1/solution.js
+++ task_1/solution.js
@@ -24,12 +24,13 @@ function addInCartMessage(title, price) {
 function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
-    let oldValue = value;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
-
+    let oldValue = value;
+    let newValue = oldValue;
+    newValue = newValue + 1;
     // Конец решения задания №1.2.
 
     return newValue;
@@ -44,7 +45,9 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽'
+    newSumText =`${newSum} ₽`
     // Конец решения задания №1.3.
 
     return newSumText;
