ba4b7745ed322fee21c25e2a27ad9242ee7c765e
Rgdft <terra.alexey@gmail.com>
2022-10-15 12:46:07 +0600

    Task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,6 +7,17 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+    let data = JSON.parse(json);
+    let products = data.products;
+    return products;
+}
+function renderProductsCards(json){
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i+=1){
+        addProduct(products[i]);
+    }
 }
 
 // Напишите функцию renderProductsCards(json)
