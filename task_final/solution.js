ec010c83e9481445002454bb560f28bcaf94fa5d
Rgdft <terra.alexey@gmail.com>
2022-10-15 20:15:52 +0600

    Final task resolved

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,7 +38,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         let goodsObj = {};
         goodsObj.title = goods[i].title;
         goodsObj.count = goods[i].count;
